b5c40928a93dddf4452b01f64dd93c3634da13fa
Denzel <lawrencedenzel60@gmail.com>
2024-06-13 16:57:58 +0300

    "Made repositores section, Added z-index to Header.css, imported and rendered Repositories component in Home.jsx, and added a div with class 'repo-users' to wrap it."

---
+++ src/Components/Repositories/Repositories.jsx
@@ -0,0 +1,89 @@
+import './Repositories.css'
+import { FaCodeFork } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
+
+function Repositories() {
+    return (
+        <div className='repo-section'>
+            <section className="repos">
+                <div className="title">
+                    <h3>Repository Name</h3>
+                    <p>Repository Description</p>
+                </div>
+
+                <div className="forks">
+                    <p>< FaCodeFork /></p>
+                    <p>< FaStar /></p>
+                </div>
+            </section>
+            <section className="repos">
+                <div className="title">
+                    <h3>Repository Name</h3>
+                    <p>Repository Description</p>
+                </div>
+
+                <div className="forks">
+                    <p>< FaCodeFork /></p>
+                    <p>< FaStar /></p>
+                </div>
+            </section>
+            <section className="repos">
+                <div className="title">
+                    <h3>Repository Name</h3>
+                    <p>Repository Description</p>
+                </div>
+
+                <div className="forks">
+                    <p>< FaCodeFork /></p>
+                    <p>< FaStar /></p>
+                </div>
+            </section>
+            <section className="repos">
+                <div className="title">
+                    <h3>Repository Name</h3>
+                    <p>Repository Description</p>
+                </div>
+
+                <div className="forks">
+                    <p>< FaCodeFork /></p>
+                    <p>< FaStar /></p>
+                </div>
+            </section>
+            <section className="repos">
+                <div className="title">
+                    <h3>Repository Name</h3>
+                    <p>Repository Description</p>
+                </div>
+
+                <div className="forks">
+                    <p>< FaCodeFork /></p>
+                    <p>< FaStar /></p>
+                </div>
+            </section>
+            <section className="repos">
+                <div className="title">
+                    <h3>Repository Name</h3>
+                    <p>Repository Description</p>
+                </div>
+
+                <div className="forks">
+                    <p>< FaCodeFork /></p>
+                    <p>< FaStar /></p>
+                </div>
+            </section>
+            <section className="repos">
+                <div className="title">
+                    <h3>Repository Name</h3>
+                    <p>Repository Description</p>
+                </div>
+
+                <div className="forks">
+                    <p>< FaCodeFork /></p>
+                    <p>< FaStar /></p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Repositories
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react"
 import Header from "../Components/Header/Header"
 import Profile from "../Components/Profile/Profile"
+import Repositories from "../Components/Repositories/Repositories"
+import './Home.css'
 
 function Home() {
     return (
         <React.Fragment>
             < Header />
             < Profile />
+            <div className="repo-users">
+                < Repositories />
+            </div>
         </React.Fragment>
     )
 }
